c06f1bbafb6e81f93e470ede7b990ab960cd6385
Ryan R <irishreid55@gmail.com>
2023-04-13 21:55:18 -0700

    working on description section in README

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
-# fhfdsf
+# fdvdfvd
+
+
+
 ## Description
-jdsnjdsf
+dfvdf
+
+## Table of Contents
+true
+
+
+
+
+## Installation
+fdfgdf
+
+
+## Usage
+undefined
+
+
+## license
+MIT
+
+
+## Contributing
+undefined
+
+
+## Tests  
+undefined
+
+
+## Questions
+undefined
+
+
+## Contact
+undefined
+
+
+## GitHub
+undefined
+
+
+## Email
+undefined
+
+
+## Credits
+undefined
+
+
+## Badges
+undefined
+
+
+
 
--- index.js
+++ index.js
@@ -2,41 +2,97 @@
 const generateMarkdown = require("./utils/generateMarkdown");
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the name of your project?"
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of your project?",
+  },
+  {
+    type: "confirm",
+    name: "tableOfContents",
+    message: "Do you want to create a table of contents for your document?",
+    default: true,
+  },
+  {
+    when: function (response) {
+      return response.tableOfContents;
     },
-    {
-        type: "input",
-        name: "description",
-        message: "Please provide a description of your project?"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Please include complete install instructions?"
-    }, 
-    {
-        type: "list",
-        name: "license",
-        message: "what license?",
-        choices: ["MIT", "Apache", "GPL", "BSD", "None"]
-    },   
-];
+    type: "checkbox",
+    name: "tableOfContentsItems",
+    message: "Which items should be in the Table of Contents?",
+    choices: [
+      "TITLE",
+      "DESCRIPTION",
+      "INSTALLATION",
+      "USAGE",
+      "CREDITS",
+      "BADGES",
+      "FEATURES",
+      "CONTRIBUTING",
+      "TESTS",
+      "QUESTIONS",
+      "CONTACT",
+      "GITHUB",
+      "EMAIL",
+    ].map(item => ({ name: item, value: item.toLowerCase() })),
+    default: [
+      "title",
+      "description",
+      "installation",
+      "usage",
+      "credits",
+      "license",
+      "badges",
+      "features",
+      "contributing",
+      "tests",
+      "questions",
+      "contact",
+      "github",
+      "email",
+    ],
+  },
 
+  {
+    when: function (response) {
+      return !response.tableOfContents;
+    },
+    type: "confirm",
+    name: "skipTableOfContents",
+    message: "Do you want to skip creating a Table of Contents?",
+    default: true,
+  },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "Please include complete install instructions?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "what license?",
+    choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((data) => {
-console.log(data);
-fs.writeFileSync("README.md", generateMarkdown(data)
-    )})
+  inquirer.prompt(questions).then(data => {
+    console.log(data);
+    fs.writeFileSync("README.md", generateMarkdown(data));
+  });
 }
 // Function call to initialize app
 init();
